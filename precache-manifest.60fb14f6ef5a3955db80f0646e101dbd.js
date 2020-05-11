e3117d8fa90b1406a81e5f1baacce7fbb6dbfcc4
Xie Zhihao <xzh1206@gmail.com>
2020-05-11 20:39:47 +0800

    Updates

---
+++ precache-manifest.60fb14f6ef5a3955db80f0646e101dbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dcf81ffaece892102457bb54a45f6d8",
+    "url": "/ikago.ikas.ink/index.html"
+  },
+  {
+    "revision": "aa83269109ce099679f5",
+    "url": "/ikago.ikas.ink/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "d900d47fa4bb9e542edb",
+    "url": "/ikago.ikas.ink/static/css/main.280cd546.chunk.css"
+  },
+  {
+    "revision": "aa83269109ce099679f5",
+    "url": "/ikago.ikas.ink/static/js/2.004ddd1c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ikago.ikas.ink/static/js/2.004ddd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d900d47fa4bb9e542edb",
+    "url": "/ikago.ikas.ink/static/js/main.f95cb73a.chunk.js"
+  },
+  {
+    "revision": "56667ed44a5d471cf533",
+    "url": "/ikago.ikas.ink/static/js/runtime-main.fefc0655.js"
+  }
+]);
